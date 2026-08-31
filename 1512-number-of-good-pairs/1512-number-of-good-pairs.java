6c3ca41ec9bf99c5bfa0ab10c88f967ee41eb199
VARSHIKA SUDALAI MURTHY SHENBAGAVEL <varshika8172@gmail.com>
2026-08-31 16:33:54 +0530

    Time: 1 ms (85.51%), Space: 42.9 MB (42.8%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -3,13 +3,17 @@ class Solution {
         HashMap<Integer,Integer> hs=new HashMap<>();
         int count=0;
         for(int i:nums){
-            if(hs.containsKey(i)) {hs.put(i,hs.get(i)+1);}
-            else {hs.put(i,1);}
+            if(hs.containsKey(i)) {
+                count = count + hs.get(i);
+                hs.put(i,hs.get(i)+1);
+                
+                }
+            else {
+                hs.put(i,1);
+            }
         }   
-        int res=0;                                    
-        for(int n:hs.keySet()){
-            count=(hs.get(n));
-            for(int i=0;i<count;i++)res+=i;
-        }return res;
+
+        return count;
+        
     }
 }
