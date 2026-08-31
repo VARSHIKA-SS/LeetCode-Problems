c0ed77ab3aef4a7107ee4f5d62053dcc6a4e2b03
VARSHIKA SUDALAI MURTHY SHENBAGAVEL <varshika8172@gmail.com>
2026-08-31 23:57:57 +0530

    Time: 14 ms (14.83%), Space: 46.3 MB (99.31%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,11 +1,18 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int check=0;
-        for(int i=0;i<nums.length;i++){
-            boolean flag=false;
-            for(int j=0;j<nums.length;j++){
-                if(i!=j && nums[i]==nums[j]){ flag=true; break;}
-            }if(!flag) return nums[i];
-        }return -1;
+        // int check=0;
+        // for(int i=0;i<nums.length;i++){
+        //     boolean flag=false;
+        //     for(int j=0;j<nums.length;j++){
+        //         if(i!=j && nums[i]==nums[j]){ flag=true; break;}
+        //     }if(!flag) return nums[i];
+        // }return -1;
+
+        HashMap<Integer,Integer> hs=new HashMap<>();
+     for(int p: nums){if(hs.containsKey(p)) hs.put(p,(hs.get(p))+1);
+     else hs.put(p,1);} 
+     for(int l:hs.keySet()){
+        if(hs.get(l)==1) return l;
+     }return -1;  
     }
 }
