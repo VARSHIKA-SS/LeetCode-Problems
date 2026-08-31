61cb9f837d4de91b99ed8b6d4ce56416e5c3fd0d
VARSHIKA SUDALAI MURTHY SHENBAGAVEL <varshika8172@gmail.com>
2026-08-31 23:49:47 +0530

    Time: 21 ms (7.3%), Space: 47.1 MB (9.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,10 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
-        else{
-            char a[]=s.toCharArray();char b[]=t.toCharArray();
-            Arrays.sort(a);Arrays.sort(b);
-            if(Arrays.equals(a,b)) return true;
-        }return false;
+        // if(s.length()!=t.length()) return false;
+        // else{
+        //     char a[]=s.toCharArray();char b[]=t.toCharArray();
+        //     Arrays.sort(a);Arrays.sort(b);
+        //     if(Arrays.equals(a,b)) return true;
+        // }return false;
+        HashMap<Character,Integer> hs1=new HashMap<>();
+        if(s.length()!=t.length())return false;
+        for(int i=0;i<s.length();i++){if (hs1.containsKey(s.charAt(i))){hs1.put(s.charAt(i),hs1.get(s.charAt(i))+1);}
+        else hs1.put(s.charAt(i),1);}
+        HashMap<Character,Integer> hs2=new HashMap<>();
+        for(int i=0;i<t.length();i++){if (hs2.containsKey(t.charAt(i))){hs2.put(t.charAt(i),hs2.get(t.charAt(i))+1);}
+        else hs2.put(t.charAt(i),1);}
+        if(hs1.equals(hs2)) return true;else return false;
     }
 }
