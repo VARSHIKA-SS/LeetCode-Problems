c4f6197c12419594a9e27d266e3f48ebed2a7fd4
VARSHIKA SUDALAI MURTHY SHENBAGAVEL <varshika8172@gmail.com>
2026-08-31 20:46:05 +0530

    Time: 2 ms (97.29%), Space: 44.8 MB (33.09%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,10 +1,17 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr.length;j++){
-                if((i!=j) && arr[i]==2*arr[j])
-                return true;
-            }
-        }return false;
+        // for(int i=0;i<arr.length;i++){
+        //     for(int j=0;j<arr.length;j++){
+        //         if((i!=j) && arr[i]==2*arr[j])
+        //         return true;
+        //     }
+        // }return false;
+        HashMap<Integer,Integer> hs=new HashMap<>();
+        for(int i:arr){            
+            if(i%2==0 && hs.containsKey(i/2)) return true;
+            if(hs.containsKey(i*2)) return true;
+            else {hs.put(i,i*2);}
+        }
+        return false;
     }
 }
