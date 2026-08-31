8eb84da7cb211d26e97d031914ed2a19e82490c2
VARSHIKA SUDALAI MURTHY SHENBAGAVEL <varshika8172@gmail.com>
2026-08-31 15:19:11 +0530

    Time: 2 ms (99.34%), Space: 47.5 MB (11.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,10 +1,20 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-               if(nums[i]+nums[j]==target) 
-               return new int []{i,j};
-        }
-    }return new int []{};
-}
+    public int[] twoSum(int[] arr, int target) {
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //     if(nums[i]+nums[j]==target)
+        //     return new int[]{i,j};
+        //     }
+        // }return new int[]{};
+        HashMap<Integer, Integer> hs=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            int b=target-arr[i];
+            if(hs.containsKey(b)){
+                return new int[]{i,hs.get(b)};
+            }
+            else{
+                hs.put(arr[i],i);
+            }
+        }return new int[]{};
+    }
 }
