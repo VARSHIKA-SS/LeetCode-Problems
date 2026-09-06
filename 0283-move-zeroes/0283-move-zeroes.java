02ce3a718dda53aa65fee4cdcdae7b8cd98120b5
VARSHIKA SUDALAI MURTHY SHENBAGAVEL <varshika8172@gmail.com>
2026-09-06 21:37:40 +0530

    Time: 2 ms (91.78%), Space: 47.8 MB (38.16%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,13 +1,19 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int left=0;
-            for(int ryt=0;ryt<nums.length;ryt++){
-                if(nums[ryt]!=0){
-                    int temp=nums[left];
-                    nums[left]=nums[ryt];
-                    nums[ryt]=temp;
-                    left++;
-                }
+
+        int left = 0;
+
+        for (int ryt = 0; ryt < nums.length; ryt++) {
+
+            if (nums[ryt] != 0) {
+                nums[left] = nums[ryt];
+                left++;
             }
+        }
+
+        while (left < nums.length) {
+            nums[left] = 0;
+            left++;
+        }
     }
 }
