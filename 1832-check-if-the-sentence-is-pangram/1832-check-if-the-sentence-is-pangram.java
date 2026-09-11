3517027137f9753ed66bfbae8435c6f24b23a63c
VARSHIKA SUDALAI MURTHY SHENBAGAVEL <varshika8172@gmail.com>
2026-09-11 23:11:57 +0530

    Time: 6 ms (5.12%), Space: 43 MB (33.38%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        HashSet<Character> hs=new HashSet<>();
+        for(int i=0;i<sentence.length();i++){
+            if("abcdefghijklmnopqrstuvwxyz".indexOf(sentence.charAt(i))!=-1){
+                hs.add(sentence.charAt(i));
+            }
+        }if(hs.size()==26) return true;
+        else return false;
+    }
+}
